0c548fe406ee9e470787a65681ad746bdd7b124d
jeeeseo98 <jeeeseo98@naver.com>
2025-01-28 14:59:48 +0900

    [chore] spring boot 초기 설정 #S12P11B201-52

---
+++ backend/src/main/java/com/myaicrosoft/myonitoring/MyonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.myaicrosoft.myonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyonitoringApplication.class, args);
+	}
+
+}
