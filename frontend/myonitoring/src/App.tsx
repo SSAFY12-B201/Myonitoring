c88636afe485d10c2df382523e10a9241f430c42
CHJ <shine8648@naver.com>
2025-02-04 10:05:25 +0900

    [feat] 스플래쉬 페이지 #S12P11B201-181

--- frontend/myonitoring/src/App.tsx
+++ frontend/myonitoring/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import SplashScreen from "./pages/SplashScreen";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [showSplash, setShowSplash] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {showSplash ? (
+        <SplashScreen onFinish={() => setShowSplash(false)} />
+      ) : (
+        <main className="flex items-center justify-center h-screen">
+          <h1 className="text-3xl font-bold">메인 화면</h1>
+        </main>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
