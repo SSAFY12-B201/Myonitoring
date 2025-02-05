0a7f96932c279604dca2b16cf0a8b007baf80bf6
CHJ <shine8648@naver.com>
2025-02-05 10:21:29 +0900

    [feat] 로그인회원가입 화면 구현 #S12P11B201-86

--- frontend/myonitoring/src/App.tsx
+++ frontend/myonitoring/src/App.tsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import SplashScreen from "./pages/SplashScreen";
+import LoginSignUp from "./pages/LoginSignUp";
 
 const App: React.FC = () => {
   const [showSplash, setShowSplash] = useState(true);
 
   return (
     <>
-      {showSplash ? (
-        <SplashScreen onFinish={() => setShowSplash(false)} />
-      ) : (
-        <main className="flex items-center justify-center h-screen">
-          <h1 className="text-3xl font-bold">메인 화면</h1>
-        </main>
-      )}
+      <main>
+        <LoginSignUp />
+        {/* <h1 className="text-3xl font-bold">메인 화면</h1> */}
+      </main>
     </>
   );
 };
