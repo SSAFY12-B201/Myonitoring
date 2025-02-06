ffd132b4d21921142502ae22427973f9cba32a53
jinisili <alwaysonlylord@naver.com>
2025-02-06 17:26:31 +0900

    [jinsil] 00시에 전날 섭취량 통계 계산하는 스케줄링 초기 구현

--- backend/src/main/java/com/myaicrosoft/myonitoring/MyonitoringApplication.java
+++ backend/src/main/java/com/myaicrosoft/myonitoring/MyonitoringApplication.java
@@ -11,3 +11,21 @@ public class MyonitoringApplication {
 	}
 
 }
+package com.myaicrosoft.myonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Spring Boot 애플리케이션의 진입점 클래스
+ * - @EnableScheduling: 스케줄링 작업을 활성화합니다.
+ */
+@SpringBootApplication
+@EnableScheduling // 스케줄링 활성화
+public class MyonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyonitoringPracJpaApplication.class, args);
+	}
+}
