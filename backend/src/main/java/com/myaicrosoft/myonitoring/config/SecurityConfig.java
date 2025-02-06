3f1a0c13a3493e8583572aec98bb704d841d026c
jeeeseo98 <jeeeseo98@naver.com>
2025-02-06 14:13:06 +0900

    [chore] 보안 설정 수정

--- backend/src/main/java/com/myaicrosoft/myonitoring/config/SecurityConfig.java
+++ backend/src/main/java/com/myaicrosoft/myonitoring/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**","/api/hello").permitAll() 
+                .requestMatchers("/auth/**","/api/**").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(new JwtAuthenticationFilter(jwtProvider),
                 UsernamePasswordAuthenticationFilter.class);
