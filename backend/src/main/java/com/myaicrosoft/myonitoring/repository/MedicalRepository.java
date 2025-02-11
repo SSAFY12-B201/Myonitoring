6d82623935d3ca975822e143dcea06ebd764a02b
jinisili <alwaysonlylord@naver.com>
2025-02-11 17:11:50 +0900

    [feat] medicalepository 수정

--- backend/src/main/java/com/myaicrosoft/myonitoring/repository/MedicalRepository.java
+++ backend/src/main/java/com/myaicrosoft/myonitoring/repository/MedicalRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 의료 기록(Medical)을 관리하는 Repository 인터페이스입니다.
@@ -20,4 +21,13 @@ public interface MedicalRepository extends JpaRepository<Medical, Long> {
      * @return 해당 기간 동안의 모든 의료 기록 리스트.
      */
     List<Medical> findByCatIdAndVisitDateBetween(Long catId, LocalDate start, LocalDate end);
+
+    /**
+     * 특정 고양이의 특정 날짜 의료 기록 중 가장 최신 데이터를 조회합니다.
+     *
+     * @param catId      고양이 ID
+     * @param visitDate 방문 날짜
+     * @return 가장 최신의 의료 기록 데이터 (Optional)
+     */
+    Optional<Medical> findTopByCatIdAndVisitDateOrderByVisitTimeDesc(Long catId, LocalDate visitDate);
 }
